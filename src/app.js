3715228151f550eb103389799ff3de903ea47c06
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-12-25 11:01:10 +0300

    basic styles and controls

--- src/app.js
+++ src/app.js
@@ -1,14 +1,48 @@
-var app = new Vue({
-    el: "#app",
-    data: {
-        parts: []
-    },
-    methods: {
-        createConfig: function () {
-            return JSON.stringify(this.parts)
-        },
-        addPart: function () {
-            this.parts.push({})
-        }
+(function () {
+    function arrayMove(arr, fromIndex, toIndex) {
+        var element = arr[fromIndex];
+        arr.splice(fromIndex, 1);
+        arr.splice(toIndex, 0, element);
     }
-})
+
+    var app = new Vue({
+        el: "#app",
+        data: {
+            parts: []
+        },
+        methods: {
+            movePart: function (part, direction) {
+                let index = this.parts.indexOf(part)
+                if (index < 0) {
+                    console.log(`Can't find part = ${part}`)
+                    return
+                }
+
+                switch (direction) {
+                    case "up":
+                        arrayMove(this.parts, index, Math.max(0, index - 1))
+                        break
+
+                    case "down":
+                        arrayMove(this.parts, index, Math.min(this.parts.length - 1, index + 1))
+                        break
+
+                    default:
+                        console.log(`Unknown move part direction: ${direction}`)
+                        break
+                }
+            },
+
+            removePart: function (part) {
+
+            },
+
+            createConfig: function () {
+                return JSON.stringify(this.parts, null, 4)
+            },
+            addPart: function () {
+                this.parts.push({})
+            }
+        }
+    })
+})()
--- src/components/story-part.js
+++ src/components/story-part.js
@@ -1,8 +1,20 @@
 Vue.component("story-part", {
     template: `
-        <div>
-            <p>{{ JSON.stringify(part) }}</p>
-            <input v-model=\"part.title\">
+        <div class="story-part">
+            <div class="story-controls">
+                <div class="action-button secondary" v-on:click="$emit('move-part', part, 'up')">up</div>
+                <div class="action-button secondary" v-on:click="$emit('move-part', part, 'down')">down</div>
+            </div>
+            <div class="form">
+                <div class="field">
+                    <div class="label">Длительность (в секундах)</div>
+                    <input class="input" type="number" placeholder="15" v-model=\"part.duration\"/>
+                </div>
+              <p>{{ JSON.stringify(part) }}</p>
+            </div>
+            <div class="preview">
+                
+            </div>
         </div>
     `,
     props: ['part']
