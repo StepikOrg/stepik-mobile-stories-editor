22be0a6e517c7193687a4ac43ee2e4180cefcf06
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-12-25 22:49:43 +0300

    image uploading

--- src/components/story-part.js
+++ src/components/story-part.js
@@ -13,23 +13,63 @@ Vue.component("story-part", {
                 </div>
                 <div class="field">
                     <div class="label">Длительность (в секундах)</div>
-                    <input class="input" type="number" placeholder="15" v-model=\"part.duration\"/>
+                    <input class="input" type="number" placeholder="15" v-model="part.duration"/>
                 </div>
                 <div class="field">
                     <div class="label">Фоновая картинка</div>
-                    <input type="hidden" role="uploadcare-uploader" data-images-only />
+                    <input 
+                        class="input" 
+                        type="text" 
+                        placeholder="Картинка не выбрана"
+                        v-model="part.image"/>
+                    <div class="action-button inline mt8" v-on:click="chooseImage">Загрузить картинку</div>
+                    <div class="status" v-if="imageUploadStatus">{{ imageUploadStatus }}</div>
                 </div>
 <!--              <p>{{ JSON.stringify(part) }}</p>-->
             </div>
             <div class="preview">
-                <div class="device android">
-                  <div class="overlay top"></div>
-                  <div class="overlay bottom"></div>
-                  <div class="hint">Android</div>
+                <div class="device android" 
+                     v-bind:style="{ 'background-image': 'url(' + part.image + ')' }"
+                >
+                    <div class="overlay top"></div>
+                    <div class="overlay bottom"></div>
+                    <div class="hint">Android</div>
                 </div>
-                <div class="device ios">iOS</div>
+                <div class="device ios" 
+                     v-bind:style="{ 'background-image': 'url(' + part.image + ')' }"
+                >
+                    <div class="overlay top"></div>
+                    <div class="overlay bottom"></div>
+                    <div class="hint">iOS</div>
+                </div>
             </div>
         </div>
     `,
-    props: ['part']
+    data: function () {
+        return {imageUploadStatus: undefined}
+    },
+    props: ['part'],
+    methods: {
+        chooseImage: function () {
+            this.imageUploadStatus = undefined
+
+            this.imageUploader = uploadcare.openDialog([])
+            this.imageUploader.done((filePromise) => {
+                this.imageUploadStatus = 'Загрузка файла...'
+                filePromise.done((file) => {
+                    this.part.image = file.cdnUrl
+                    this.imageUploadStatus = undefined
+                    this.$forceUpdate()
+                })
+                filePromise.fail((err) => {
+                    this.imageUploadStatus = err
+                    this.$forceUpdate()
+                })
+            })
+            this.imageUploader.fail((err) => {
+                this.imageUploadStatus = err
+                this.$forceUpdate()
+            })
+        }
+    }
 })
